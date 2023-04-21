9cc2d096704b6325134b8607943dda90c35345ce
SanjulaD <desanjula99@gmail.com>
2023-04-21 12:44:56 +0530

    crud lambda functions

--- handler.js
+++ handler.js
@@ -2,7 +2,30 @@
 
 module.exports.createNote = async (event) => {
   return {
-    statusCode: 200,
+    statusCode: 201,
     body: JSON.stringify("A new note was created"),
+  };
+};
+
+module.exports.updateNote = async (event) => {
+  let noteId = event.pathParameters.id
+  return {
+    statusCode: 200,
+    body: JSON.stringify("The note with ID: " + noteId + " updated"),
+  };
+};
+
+module.exports.deleteNote = async (event) => {
+  let noteId = event.pathParameters.id
+  return {
+    statusCode: 200,
+    body: JSON.stringify("The note with ID: " + noteId + " deleted"),
+  };
+};
+
+module.exports.getAllNotes = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify("All notes are returned"),
   };
 };
