0d36d248dfbb6557a16bc21eaf2a00854adf36ec
SanjulaD <desanjula99@gmail.com>
2023-05-23 21:46:20 +0530

    creating notes testing

--- test/notes.test.js
+++ test/notes.test.js
@@ -2,6 +2,7 @@
 
 let init = require('./steps/init')
 let { an_authenticated_user } = require('./steps/given')
+let { we_invoke_create_note } = require('./steps/when')
 let idToken;
 
 describe('Given an authenticated uses', () => {
@@ -11,9 +12,18 @@ describe('Given an authenticated uses', () => {
         idToken = user.AuthenticationResult.IdToken
     })
 
+
     describe('When we invoke POST /notes endpoint', () => {
-        it('should create a new note', () => {
-            expect(true).toBe(true);
+        it('should create a new note', async () => {
+            const body = {
+                id: "140",
+                title: "My test note title",
+                body: "Hello this is my test note"
+            }
+
+            let result = await we_invoke_create_note({ idToken, body })
+            expect(result.statusCode).toEqual(201);
+            expect(result.body).not.toBeNull();
         });
     })
 })
